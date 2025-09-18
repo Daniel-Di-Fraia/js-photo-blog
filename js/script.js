a0d6f03b11e939fb4f8bf5bd2d56d5e027cc72d0
Daniel-Di-Fraia <daniel.difraia2600@gmail.com>
2025-09-18 13:13:21 +0200

    finalizzazione milestone 3

--- js/script.js
+++ js/script.js
@@ -7,8 +7,7 @@ const apiPics = 'https://lanciweb.github.io/demo/api/pictures/';
 //richiamiamo il container dell'overlay
 const overlay = document.querySelector(".box");
 
-//richiamiamo il bottone per la chiusura
-const bottone = document.querySelector(".btn");
+
 
 
 //generiamo le 6 immagini tramite chiamata ajax
@@ -17,8 +16,9 @@ axios.get(apiPics).then((response) => {
     const cardImg = response.data;
     // console.log(cardImg);
     
-    //creiamo una variabile contenitore per le cards
+    //creiamo una variabile contenitore per le cards e overlay img
     let card = "";
+    let over = "";
 
     //utilizziamo i dati per creare le cards
     cardImg.forEach(singleImg => {
@@ -26,7 +26,7 @@ axios.get(apiPics).then((response) => {
 
         //per ogni pic/card aggiungiamo un pezzo alla nostra sezione
         card += `
-            <div class="col-main">
+            <div class="col-main" data-url="${singleImg.url}" data-title="${singleImg.title}">
                 <img class="pics" src="${singleImg.url}" alt="${singleImg.title}">
                 <aside class="data">${singleImg.date}</aside>
                 <aside class="evento">${singleImg.title}</aside>
@@ -41,16 +41,34 @@ axios.get(apiPics).then((response) => {
     const allCards = document.querySelectorAll(".col-main");
     console.log(allCards);
 
-    //ogni card avrà un click event
     allCards.forEach(singleCard => {
         singleCard.addEventListener("click", () => {
+            //prendiamo i dati necessari per l overlay direttamente dal DOM tramite attributo
+            //(visto in documentazione)
+            const imgUrl = singleCard.dataset.url;
+            const titleImg = singleCard.dataset.title;
+
+        //per ogni pic/card sull overlay aggiungiamo HTML in pagina
+        over = `<img src="${imgUrl}" alt="${titleImg}" class="bigImg">
+                <button class="btn">Chiudi</button>`;
+            // console.log(over);
+
+            //definiamo output tramite stringa in HTML
+            overlay.innerHTML = over;
+
+            //rimuoviamo la classe "d-none" cosi da far apparire il nostro overlay in pagina
             overlay.classList.remove("d-none");
+
+            //pulsante per chiudere l'overlay
+            //richiamiamo il bottone per la chiusura
+            const bottone = document.querySelector(".btn");
             bottone.addEventListener("click", () => {
                 overlay.classList.add("d-none");
             });
         });
     });
 });
+
 
 
     
